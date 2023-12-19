99fcaef9480af1cee98a849ccbd16935a05f1b24
Kutniak <maxim.kutnyak@gmail.com>
2023-12-19 14:34:01 +0200

    creating project

---
+++ src/main/java/com/example/SpringBootBloodBankManagementSystem/SpringBootBloodBankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootBloodBankManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBloodBankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBloodBankManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootBloodBankManagementSystem/SpringBootBloodBankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootBloodBankManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBloodBankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
